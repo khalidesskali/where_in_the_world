928a60c5be066df3f9d61b701f409e1c85aae72a
khalidesskali <esskalikhalid7@gmail.com>
2025-03-25 11:18:58 +0000

    Implement darkMode feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,72 @@
-import React from "react";
+import { p } from "framer-motion/client";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [darkmode, setDarkmode] = useState(() => {
+    localStorage.getItem === "dark";
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (darkmode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkmode]);
+
   return (
-    <div className="h-20 shadow-sm dark:bg-dark-blue">
+    <div className="h-20 shadow-sm dark:bg-dark-blue dark:text-white">
       <div className="flex items-center justify-between container mx-auto p-4 h-full">
-        <h2 className="font-bold md:text-xl lg:text-2xl">
+        <h2
+          className="font-bold md:text-xl lg:text-2xl cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           Where in the world?
         </h2>
-        <div className="flex items-center gap-[6px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5 dark:fill-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753
-                9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-            />
-          </svg>
-          <span>Dark Mode</span>
-        </div>
+
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            setDarkmode(!darkmode);
+          }}
+        >
+          {darkmode ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753
+     9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
+            </svg>
+          )}
+        </button>
       </div>
     </div>
   );
